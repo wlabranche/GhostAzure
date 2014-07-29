04440daaa75b913cf2d38cc3a5985d115fe51a8d
Will LaBranche <wlabranche@gmail.com>
2014-07-29 09:34:50 -0700

    updated url

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://my-ghost-blog.com',
+        url: 'http://www.labranche.io',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://www.labranche.io',
         mail: {},
         database: {
             client: 'sqlite3',
